ab633564dfe4c8bf01a691bbaa1647d4a365e85d
Mirzohidde4 <mirzohidxudayberganov27@gmail.com>
2024-07-20 12:25:38 +0500

    bootstrap

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_bootstrap5',
+    'crispy_forms',
     'accounts',
 ]
 
@@ -118,3 +120,5 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
